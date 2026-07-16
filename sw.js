972f7dc34bc8773a7bb0f5c5b48ee83053cb4f85
RICARDO210394 <bohorquez-ricardo9628@unesum.edu.ec>
2026-07-16 15:26:34 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 // Service Worker - Cámaras Piazza
-// VERSION: 20260716200729
-const CACHE_NAME = 'camaras-piazza-20260716200729';
-const ARCHIVOS_ESENCIALES = ['/', '/index.html'];
+// VERSION: 20260716202401
+const CACHE_NAME = 'camaras-piazza-20260716202401';
+const URL_BASE = 'https://ricardo210394.github.io/reporte-camaras-piazza';
+const ARCHIVOS_ESENCIALES = [URL_BASE + '/', URL_BASE + '/index.html'];
 self.addEventListener('install', event => { self.skipWaiting(); });
 self.addEventListener('activate', event => {
   event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim()));
