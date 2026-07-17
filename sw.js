5b25e948180e45996db0e7884438c8f22a79fa8b
RICARDO210394 <bohorquez-ricardo9628@unesum.edu.ec>
2026-07-16 23:32:11 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// SW VERSION: 20260717042248
-const CACHE_NAME = 'camaras-20260717042248';
+// SW VERSION: 20260717043019
+const CACHE_NAME = 'camaras-20260717043019';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(k=>Promise.all(k.map(x=>caches.delete(x)))).then(()=>self.clients.claim())); });
 self.addEventListener('fetch', e => { if(!e.request.url.startsWith(self.location.origin))return; e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))); });
